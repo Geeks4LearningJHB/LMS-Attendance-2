8309674da33f798305582003531b4ae2ab0a50e9
dev.kgaugelomth <121237206+code-geek15@users.noreply.github.com>
2024-03-11 10:01:53 +0200

    structure fixed

--- Attendance-SpringBoot-Backend/src/main/java/com/geeks/AttendanceSpringBootBackend/repository/LeaveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.geeks.AttendanceSpringBootBackend.repository;
-
-import com.geeks.AttendanceSpringBootBackend.entity.Leave;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LeaveRepository extends JpaRepository<Leave, Long> {
-}

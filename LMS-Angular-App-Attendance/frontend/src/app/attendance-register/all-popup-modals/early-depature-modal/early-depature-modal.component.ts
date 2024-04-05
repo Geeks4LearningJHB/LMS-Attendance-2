1250253489e7d154bce378120477134ffce8869d
Aubrey <mashabaubrey@gmail.com>
2024-04-05 08:56:19 +0200

    date display

--- LMS-Angular-App-Attendance/frontend/src/app/attendance-register/all-popup-modals/early-depature-modal/early-depature-modal.component.ts
+++ LMS-Angular-App-Attendance/frontend/src/app/attendance-register/all-popup-modals/early-depature-modal/early-depature-modal.component.ts
@@ -11,21 +11,19 @@ import { AttendanceModel } from '../../models/attendance.interface';
 export class EarlyDepatureModalComponent implements OnInit {
 
   earlyDepartures: AttendanceModel[] = []
-  currentDate!: Date;
+  yesterdayDate!: Date;
+  currentDate!:Date;
  constructor(public dialogRef: MatDialogRef<EarlyDepatureModalComponent>,
               @Inject(MAT_DIALOG_DATA) public data: AttendanceModel[],
                private attendanceService:AttendanceService) {}
-               //  absentStudents: { name: string, phoneNumber: string, sponsor: string, date: Date , reason: string}[] = [
-  //   { name: 'Neo Moloi', phoneNumber: '123-456-7890', sponsor: 'FNB', date: new Date('2022-04-05'), reason: "Took a half day." },
-  //   { name: 'Themba Zwane', phoneNumber: '123-456-7890', sponsor: 'ABSA', date: new Date('2022-04-05'),  reason: "Was not feeling ok." },
-  //   { name: 'Thembinkosi Lorch', phoneNumber: '123-456-7890', sponsor: 'FNB', date: new Date('2022-04-05'),  reason: "Felt like leaving early." },
-  //   { name: 'Rowen Willams', phoneNumber: '123-456-7890', sponsor: 'Riverside', date: new Date('2022-04-05') ,  reason: "Tired of code not running!"},
-  //   { name: 'Grant Kekana', phoneNumber: '123-456-7890', sponsor: 'AWS', date: new Date('2022-04-05') ,  reason: "Was a bit upset."},
-  // ];
+  
  
   ngOnInit(): void {
-   if (this.data && this.data.length > 0) {
     this.currentDate = new Date();
+    this.yesterdayDate = new Date();
+    this.yesterdayDate.setDate(this.currentDate.getDate() - 1);
+    
+   if (this.data && this.data.length > 0) {
       this.earlyDepartures = this.data
       console.log(this.earlyDepartures)
     }

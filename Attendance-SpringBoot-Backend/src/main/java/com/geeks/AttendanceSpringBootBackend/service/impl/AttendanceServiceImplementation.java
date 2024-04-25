ad4893d703c219bcb62943b163320fa1a1e961dd
Aubrey <mashabaubrey@gmail.com>
2024-04-25 16:22:36 +0200

    sort

--- Attendance-SpringBoot-Backend/src/main/java/com/geeks/AttendanceSpringBootBackend/service/impl/AttendanceServiceImplementation.java
+++ Attendance-SpringBoot-Backend/src/main/java/com/geeks/AttendanceSpringBootBackend/service/impl/AttendanceServiceImplementation.java
@@ -142,7 +142,6 @@ public class AttendanceServiceImplementation implements AttendanceInterface {
     public AttendanceResponseDto getAttendanceRecordById(long id) {
         Optional<AttendanceRecord> attendanceRecordOptional = attendanceRepository.findById(id);
         if (attendanceRecordOptional.isPresent()) {
-
             return  attendanceDtoMapper.mapToDto(attendanceRecordOptional.get());
         } else {
             // handle not found scenario
@@ -181,7 +180,12 @@ public class AttendanceServiceImplementation implements AttendanceInterface {
     @Override
     public List<AttendanceResponseDto> getAllUserAttendances(long userId) {
 
-         List<AttendanceRecord> attendanceRecords  =  attendanceRepository.findByUserIdUserId(userId);
+         List<AttendanceRecord> attendanceRecords  =  attendanceRepository.findAllByUserIdUserIdOrderByDateDesc(userId);
+        System.out.println("Records");
+
+        for (AttendanceRecord att : attendanceRecords){
+             System.out.println(att);
+         }
          return attendanceDtoMapper.mapToResponseDtoList(attendanceRecords);
 
     }
@@ -190,7 +194,7 @@ public class AttendanceServiceImplementation implements AttendanceInterface {
     public List<AttendanceResponseDto> getTodayAttendance() {
         date  =  timeFetcherApi.getCurrentDateInSouthAfrica();
         currentDate = LocalDate.parse(date , formatDate);
-        List<AttendanceRecord> attendanceRecords  =  attendanceRepository.findAttendanceByDate(currentDate);
+        List<AttendanceRecord> attendanceRecords  =  attendanceRepository.findAttendanceByDateOrderByLogInTimeDesc(currentDate);
         return attendanceDtoMapper.mapToResponseDtoList(attendanceRecords);
 
 
@@ -205,10 +209,11 @@ public class AttendanceServiceImplementation implements AttendanceInterface {
         currentTime = LocalTime.parse(time , formatter);
         if (attendanceRecordOptional.isPresent()) {
             AttendanceRecord attendanceRecord = attendanceRecordOptional.get();
-            if (attendanceRecord.getLogOutTime() != null) {
+            if (attendanceRecord.getLogOutTime() != null && attendanceRecord.isScanned()) {
                 throw new AttendanceExceptions("Log out time already set");
             }
             attendanceRecord.setLogOutTime(currentTime);
+            attendanceRecord.setScanned(true);
             attendanceRepository.save(attendanceRecord);
             AttendanceRecord updatedAttendanceRecord = attendanceRepository.save(attendanceRecord);
             return attendanceDtoMapper.mapToDto(updatedAttendanceRecord);
@@ -253,7 +258,7 @@ public class AttendanceServiceImplementation implements AttendanceInterface {
     }
 
     public List<AttendanceResponseDto> allEarly(LocalDate date) {
-        List<AttendanceRecord> records = attendanceRepository.findAttendanceByDate(date);
+        List<AttendanceRecord> records = attendanceRepository.findAttendanceByDateOrderByLogInTimeDesc(date);
         List<AttendanceRecord> earlyLogOuts = new ArrayList<AttendanceRecord>();
 
         for (AttendanceRecord attRecords : records) {
@@ -357,7 +362,7 @@ public class AttendanceServiceImplementation implements AttendanceInterface {
         date  =  timeFetcherApi.getCurrentDateInSouthAfrica();
         currentDate = LocalDate.parse(date , formatDate);
 
-        List<AttendanceRecord> records = attendanceRepository.findAttendanceByDate(currentDate);
+        List<AttendanceRecord> records = attendanceRepository.findAttendanceByDateOrderByLogInTimeDesc(currentDate);
         List<AttendanceRecord> lateComers = new ArrayList<AttendanceRecord>();
         for (AttendanceRecord attRecords : records) {
             if (attRecords.getStatus().equals(Status.LATE)){
